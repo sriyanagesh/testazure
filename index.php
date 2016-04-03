69b545cae46a394d3bee09dc05a785f7acc92c32
sriyanagesh <sriyanagesh@gmail.cm>
2016-04-03 13:17:43 -0500

    fifth commit

--- index.php
+++ index.php
@@ -3,20 +3,30 @@
 <?php
 								$serverName = "joaquinateam12.database.windows.net";
 								$connectionInfo = array( "Database"=>"Joaquinateam12", "UID"=>"admin12@joaquinateam12", "PWD"=>"Trotters12");
-								$conn = mssql_connect( $serverName, 'admin12@joaquinateam12','Trotters12');
+								$conn = sqlsrv_connect( $serverName, $connectionInfo);
 
 								if( $conn ) {
 								echo "Connection established.<br />";
 								}
-								mssql_select_db('joaquinateam12',$conn);
+								else {
+								die(print_r(sqlsrv_errors(), true));
+								}
+								
+							//	mssql_select_db('joaquinateam12',$conn);
 
 								$sql = "select * from dbo.Dentist";
-								$stmt = mssql_query($sql,$conn);
-								$row= mssql_fetch_row($stmt);
-								echo "before foreach";
-								foreach($row as $s){
-									echo $s;
+								$stmt = sqlsrv_query($conn, $sql);
+								if( !$stmt) {
+								die(print_r(sqlsrv_errors(), true));
 								}
+								while($row = sqlsrv_fetch_array($stmt,SQLSRV_FETCH_ASSOC)){
+								print_r $row;
+								}
+					//			$row= mssql_fetch_row($stmt);
+					//			echo "before foreach";
+					//			foreach($row as $s){
+					//				echo $s;
+					//			}
 								echo "hello mummy!";
 
 ?></body></html>
